bfc05ef058e5c5043a03a8628ad1e19e8bbe98e0
young kim <ykim878@gatech.edu>
2021-12-04 18:05:28 -0500

    .

--- linechart.js
+++ linechart.js
@@ -157,7 +157,7 @@ function LineChart(file_path) {
         .call(yAxis2);
       
 
-      // Initialize line with group a
+      // Initialize line for the first line chart
       var line = svg
         .append('g')
         .append("path")
@@ -265,9 +265,9 @@ function LineChart(file_path) {
       svg2.append("circle").attr("cx",width).attr("cy",30).attr("r", 4).style("fill", d4.schemeSet1[0])
       svg2.append("circle").attr("cx",width).attr("cy",60).attr("r", 4).style("fill", d4.schemeSet1[1])
       svg2.append("circle").attr("cx",width).attr("cy",90).attr("r", 4).style("fill", d4.schemeSet1[2])
-      svg2.append("text").attr("x", width+5).attr("y", 30).text("Accuracy").style("font-size", "15px").attr("alignment-baseline","middle")
-      svg2.append("text").attr("x", width+5).attr("y", 60).text("Recall").style("font-size", "15px").attr("alignment-baseline","middle")
-      svg2.append("text").attr("x", width+5).attr("y", 90).text("Precision").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg2.append("text").attr("x", width+5).attr("y", 230).text("Accuracy").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg2.append("text").attr("x", width+5).attr("y", 260).text("Recall").style("font-size", "15px").attr("alignment-baseline","middle")
+      svg2.append("text").attr("x", width+5).attr("y", 290).text("Precision").style("font-size", "15px").attr("alignment-baseline","middle")
 
 
       /***********************************************************************************/
